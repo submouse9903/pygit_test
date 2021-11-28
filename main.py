7af92bbe4cf69297e211cc54b5bde18dd8eb722d
submouse9903 <submouse9903@gmail.com>
2021-11-28 13:07:36 +0900

    test2

--- main.py
+++ main.py
@@ -8,9 +8,15 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     print("hello")
-
+def fibona(n):
+    if(n==0):
+        return 0
+    elif(n==1):
+        return 1
+    return fibona(n-1)+fibona(n-2)
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    print(fibona(10))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
